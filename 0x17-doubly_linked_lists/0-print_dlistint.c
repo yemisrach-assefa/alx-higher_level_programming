dc198c074b2eb3b6ec70cec55788368904dacd13
yemisrach <yemisrachassfa@gmail.com>
2022-12-08 05:32:53 -0800

    singly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,20 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    tmp = []
-    for x in matrix:
-        tmp.append(list(map(lambda x: x**2, x)))
-    return (tmp)
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: doubly linkedlist
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t num_nodes = 0;
+
+	while (h)
+	{
+		printf("%i\n", h->n);
+		num_nodes++;
+		h = h->next;
+	}
+
+	return (num_nodes);
+}
