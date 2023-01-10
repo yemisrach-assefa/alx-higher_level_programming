85f1b781d177314922265033c007db8974370ea7
yemisrach <yemisrachassfa@gmail.com>
2023-01-09 18:33:36 -0800

    python inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/python3
+"""
+lookup - returns the list of available attributes and methods of an object
+@obj: object
+Return: list object
+"""
+
+
+def lookup(obj):
+    """ return list of obj """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/python3
+"""
+1-my_list module
+"""
+
+
+class MyList(list):
+    """
+    MyList child of list
+    """
+    def print_sorted(self):
+        """
+        print_sorted - prints the list, but sorted
+        """
+        print(sorted(self))
