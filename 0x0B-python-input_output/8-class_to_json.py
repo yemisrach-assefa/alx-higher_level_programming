d017eba609e5717fea385f5c552768c97f19c0de
yemisrach <yemisrachassfa@gmail.com>
2023-01-10 14:48:08 -0800

    task

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/python3
+"""
+Module for class_to_json method.
+"""
+
+
+def class_to_json(obj):
+    """
+        returns dictionary representation with simple data structure.
+    """
+    return obj.__dict__
